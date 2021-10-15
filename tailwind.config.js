b10077a224bda972c7ac4281281d32a26044c9d0
Jordy Slagter <slagterj2001@gmail.com>
2021-10-15 21:22:23 +0200

    Initial commit of project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.svelte", "./src/**/*.js"],
+    enabled: true,
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const sveltePath = path.join(__dirname, "node_modules", "svelte");
+
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: [path.join(__dirname, "src", "index.js")],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      filename: "index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      svelte: sveltePath,
+    },
+    extensions: [".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js",
+    clean: true, // Clean the dist/ directory each time the project builds
+  },
+  devServer: {
+    hot: false, // Disable hot reloading here as Svelte3 doesn't support it (yet)
+    port: 3000,
+  },
+  module: {
+    rules: [
+      // ***** JAVASCRIPT *****
+      {
+        test: /\.(?:svelte|m?js)$/,
+        include: [path.join(__dirname, "src"), path.dirname(sveltePath)],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: "ie >= 9",
+                },
+              ],
+            ],
+          },
+        },
+      },
+      // ***** SVELTE *****
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: "svelte-loader",
+          options: {
+            emitCss: true,
+            hotReload: true,
+            compilerOptions: {
+              // Enable devtools if the node_env is development
+              dev: mode === "development",
+            },
+          },
+        },
+      },
+      // ***** STYLES *****
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      // ***** FILES *****
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
